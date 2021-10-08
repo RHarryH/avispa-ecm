8a7488ab8f08059cdfe6e30fa955f3c9da6f64aa
Rafał Hiszpański <rharryh@gmail.com>
2021-10-08 23:28:48 +0200

    Replaced original implementation of expression resolver due to issues with parsing some cases, ANTLR4 parser is now used Added more positive tests to expression resolver

--- src/main/java/com/avispa/ecm/util/expression/ExpressionResolver.java
+++ src/main/java/com/avispa/ecm/util/expression/ExpressionResolver.java
@@ -1,13 +1,15 @@
 package com.avispa.ecm.util.expression;
 
+import com.avispa.cms.util.expression.parser.ExpressionsLexer;
+import com.avispa.cms.util.expression.parser.ExpressionsParser;
 import com.avispa.ecm.model.document.Document;
+import com.avispa.ecm.util.expression.visitor.ExpressionVisitor;
 import lombok.extern.slf4j.Slf4j;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.Lexer;
+import org.antlr.v4.runtime.tree.ParseTree;
 import org.springframework.stereotype.Component;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  * @author Rafał Hiszpański
@@ -15,76 +17,18 @@ import java.util.regex.Pattern;
 @Slf4j
 @Component
 public class ExpressionResolver {
-    // function must start with upper or lower case letter, later digits are allowed,
-    private static final String FUNCTION_REGEX = "\\$[A-Za-z]+[0-9]*\\((.*)\\)";
-    private static final String FUNCTION_PARAMS_REGEX = "([^,]+\\(.+?\\))|([^,]+)";
-
-    private final Pattern functionPattern = Pattern.compile(FUNCTION_REGEX);
-    private final Pattern functionParamsPattern = Pattern.compile(FUNCTION_PARAMS_REGEX);
-
     public String resolve(Document document, String expression) {
-        return resolveFunctions(document, expression);
-    }
+        // create lexer and get tokens
+        Lexer lexer = new ExpressionsLexer(CharStreams.fromString(expression));
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
 
-    /**
-     * Checks if provided expression contains functions and tries to resolve them
-     * @param document context document
-     * @param expression expression to resolve
-     * @return
-     */
-    private String resolveFunctions(Document document, String expression) {
-        final StringBuilder sb = new StringBuilder(expression.length());
+        // run parser
+        ExpressionsParser parser = new ExpressionsParser(tokens);
+        ParseTree tree = parser.start();
 
-        Matcher m = functionPattern.matcher(expression);
+        // go through parse tree
+        ExpressionVisitor visitor = new ExpressionVisitor(document);
 
-        while (m.find()) {
-            String functionSignature = m.group();
-            log.info("Function '{}' found", functionSignature);
-
-            String functionName = getFunctionName(functionSignature);
-            String[] functionParams = getFunctionParams(document, m.group(1));
-
-            String returnedValue = FunctionFactory.resolve(functionName, functionParams, document);
-            if(null != returnedValue) {
-                m.appendReplacement(sb, returnedValue);
-            } else {
-                log.error("Can't resolve '{}' function", functionSignature);
-            }
-
-        }
-        m.appendTail(sb);
-
-        return sb.toString();
-    }
-
-    /**
-     * Retrieves function name from it's signature
-     * @param signature
-     * @return
-     */
-    private String getFunctionName(String signature) {
-        return signature.substring(1, signature.indexOf("("));
-    }
-
-    /**
-     * Returns array of function parameters retrieved from expression.
-     * If parameter is another function or mix of functions, they will be resolved first
-     * @param document context document
-     * @param params function parameters extracted from the expression
-     * @return
-     */
-    private String[] getFunctionParams(Document document, String params) {
-        List<String> functionParams = new ArrayList<>();
-        Matcher m = functionParamsPattern.matcher(params);
-
-        while (m.find()) {
-            String paramValue = resolveFunctions(document, m.group().trim()); // if parameter contains functions, resolve them first
-
-            functionParams.add(paramValue);
-            if(log.isDebugEnabled()) {
-                log.debug("Function parameter found: {}", paramValue);
-            }
-        }
-        return functionParams.toArray(new String[0]);
+        return visitor.visit(tree);
     }
 }
--- src/test/java/com/avispa/ecm/util/expression/ExpressionResolverTest.java
+++ src/test/java/com/avispa/ecm/util/expression/ExpressionResolverTest.java
@@ -1,9 +1,15 @@
 package com.avispa.ecm.util.expression;
 
+import org.antlr.v4.runtime.Parser;
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.Trees;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -28,46 +34,86 @@ class ExpressionResolverTest {
 
     @Test
     void applyFieldFromSuperDocument() {
-        assertEquals("ABC_XY_ABC", expressionResolver.resolve(document, "ABC_$value(extraField)_ABC"));
+        assertEquals("ABC_XY_ABC", expressionResolver.resolve(document, "'ABC_' + $value('extraField') + '_ABC'"));
     }
 
     @Test
     void applyNonExistingField() {
-        assertEquals("ABC__ABC", expressionResolver.resolve(document, "ABC_$value(nonExistingField)_ABC"));
+        assertEquals("ABC__ABC", expressionResolver.resolve(document, "'ABC_' + $value('nonExistingField') + '_ABC'"));
     }
 
     @Test
     void replaceInteger() {
-        assertEquals("ABC_5_ABC", expressionResolver.resolve(document, "ABC_$value(extraInt)_ABC"));
+        assertEquals("ABC_5_ABC", expressionResolver.resolve(document, "'ABC_' + $value('extraInt') + '_ABC'"));
     }
 
     @Test
     void functionOnly() {
-        assertEquals("XY", expressionResolver.resolve(document, "$value(extraField)"));
+        assertEquals("XY", expressionResolver.resolve(document, "$value('extraField')"));
     }
 
     @Test
     void insufficientNumberOfArguments() {
-        assertThrows(IllegalArgumentException.class, () -> expressionResolver.resolve(document, "ABC_$default(nonExistingField)_ABC"));
+        assertThrows(IllegalArgumentException.class, () -> expressionResolver.resolve(document, "'ABC_' + $default('nonExistingField') + '_ABC'"));
     }
 
     @Test
     void unknownFunction() {
-        assertEquals("ABC_$unknownFunction(nonExistingField)_ABC", expressionResolver.resolve(document, "ABC_$unknownFunction(nonExistingField)_ABC"));
+        assertEquals("ABC_$unknownFunction('nonExistingField')_ABC", expressionResolver.resolve(document, "'ABC_' + $unknownFunction('nonExistingField') + '_ABC'"));
     }
 
     @Test
     void dateValueFunction() {
-        assertEquals("ABC_10_ABC", expressionResolver.resolve(document, "ABC_$datevalue(extraDate, MM)_ABC"));
+        assertEquals("ABC_10_ABC", expressionResolver.resolve(document, "'ABC_' + $datevalue('extraDate', 'MM') + '_ABC'"));
     }
 
     @Test
     void dateValueFunctionInvalidFormat() {
-        assertThrows(IllegalArgumentException.class, () -> expressionResolver.resolve(document, "ABC_$datevalue(extraDate, invalid_format)_ABC"));
+        assertThrows(IllegalArgumentException.class, () -> expressionResolver.resolve(document, "'ABC_' + $datevalue('extraDate', 'invalid_format') + '_ABC'"));
     }
 
     @Test
     void nestedFunction() {
-        assertEquals("ABC_XY_ABC", expressionResolver.resolve(document, "ABC_$default($value(extraField), This is default value)_ABC"));
+        assertEquals("ABC_XY_ABC", expressionResolver.resolve(document, "'ABC_' + $default($value('extraField'), 'This is default value') + '_ABC'"));
+    }
+
+    @Test
+    void nestedFunctionWithConcatenation() {
+        assertEquals("ABC_XY", expressionResolver.resolve(document, "$default('ABC_'+$value('extraField'), 'This is default value')"));
+    }
+
+    @Test
+    void twoFunctions() {
+        assertEquals("ABC_XY", expressionResolver.resolve(document, "$value('objectName') + '_' + $default($value('extraField'), 'This is default value')"));
+    }
+
+    @Test
+    void noParamsFunction() {
+        assertThrows(IllegalArgumentException.class, () -> expressionResolver.resolve(document, "$value()"));
+    }
+
+    @Test
+    void tooManyParamsFunction() {
+        assertEquals("XY", expressionResolver.resolve(document, "$value('extraField', 'redundantParameter')"));
+    }
+
+    public static String printSyntaxTree(Parser parser, ParseTree root) {
+        StringBuilder buf = new StringBuilder();
+        recursive(root, buf, 0, Arrays.asList(parser.getRuleNames()));
+        return buf.toString();
+    }
+
+    private static void recursive(ParseTree aRoot, StringBuilder buf, int offset, List<String> ruleNames) {
+        buf.append("  ".repeat(Math.max(0, offset)));
+
+        buf.append(Trees.getNodeText(aRoot, ruleNames)).append("\n");
+        if (aRoot instanceof ParserRuleContext) {
+            ParserRuleContext prc = (ParserRuleContext) aRoot;
+            if (prc.children != null) {
+                for (ParseTree child : prc.children) {
+                    recursive(child, buf, offset + 1, ruleNames);
+                }
+            }
+        }
     }
 }
