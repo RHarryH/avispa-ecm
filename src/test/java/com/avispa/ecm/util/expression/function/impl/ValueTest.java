a8c476da698515130ff153cd44718c0b9c431871
Rafał Hiszpański <rharryh@gmail.com>
2021-10-10 22:51:15 +0200

    Added support for LocalDate in $datevalue function Updated tests

--- src/test/java/com/avispa/ecm/util/expression/function/impl/ValueTest.java
+++ src/test/java/com/avispa/ecm/util/expression/function/impl/ValueTest.java
@@ -19,7 +19,7 @@ class ValueTest {
     static void init() {
         document = new SuperDocument();
         document.setObjectName("ABC");
-        document.setExtraDate(LocalDateTime.of(2021, 10, 11, 10, 54, 18));
+        document.setExtraDateTime(LocalDateTime.of(2021, 10, 11, 10, 54, 18));
         document.setExtraInt(5);
     }
 
@@ -35,7 +35,7 @@ class ValueTest {
 
     @Test
     void getDateValue() {
-        assertEquals("2021-10-11T10:54:18", value.resolve(document, new String[] {"extraDate"}));
+        assertEquals("2021-10-11T10:54:18", value.resolve(document, new String[] {"extraDateTime"}));
     }
 
     @Test
