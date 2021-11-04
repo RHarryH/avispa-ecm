68490a4b1d52928a3bd3047ed756d6be8ce6e6d2
Rafał Hiszpański <rharryh@gmail.com>
2021-11-04 16:31:07 +0100

    Renamed Property to Control and split to two types: Organization Control (labels, separators) and Property Control (actual editable properties) Added missing Property Page type and fixed Anutoname type Updated Folder property page Added Document property page Added tests for property page

--- src/main/java/com/avispa/ecm/model/configuration/propertypage/PropertyPage.java
+++ src/main/java/com/avispa/ecm/model/configuration/propertypage/PropertyPage.java
@@ -1,12 +1,14 @@
 package com.avispa.ecm.model.configuration.propertypage;
 
 import com.avispa.ecm.model.configuration.EcmConfigObject;
-import com.avispa.ecm.model.configuration.propertypage.property.Property;
+import com.avispa.ecm.model.configuration.propertypage.controls.Control;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.OneToMany;
 import javax.persistence.OrderColumn;
 import java.util.List;
@@ -19,7 +21,7 @@ import java.util.List;
 @Setter
 @ToString(callSuper = true)
 public class PropertyPage extends EcmConfigObject {
-    @OneToMany
+    @OneToMany(cascade = {CascadeType.ALL}, fetch = FetchType.EAGER)
     @OrderColumn
-    private List<Property> properties;
+    private List<Control> controls;
 }
--- src/main/java/com/avispa/ecm/util/reflect/PropertyUtils.java
+++ src/main/java/com/avispa/ecm/util/reflect/PropertyUtils.java
@@ -1,6 +1,6 @@
 package com.avispa.ecm.util.reflect;
 
-import com.avispa.ecm.model.document.Document;
+import com.avispa.ecm.model.EcmObject;
 import lombok.extern.slf4j.Slf4j;
 
 import java.beans.IntrospectionException;
@@ -18,31 +18,33 @@ public class PropertyUtils {
 
     }
 
-    public static Object getPropertyValue(Document document, String propertyName) {
+    public static Object getPropertyValue(EcmObject ecmObject, String propertyName) {
+        // TODO: move to ReflectUtils
         try {
-            for (PropertyDescriptor pd : Introspector.getBeanInfo(document.getClass()).getPropertyDescriptors()) {
+            for (PropertyDescriptor pd : Introspector.getBeanInfo(ecmObject.getClass()).getPropertyDescriptors()) {
                 if (pd.getReadMethod() != null && !"class".equals(pd.getName()) &&
                         pd.getName().equals(propertyName)) {
-                    return pd.getReadMethod().invoke(document);
+                    return pd.getReadMethod().invoke(ecmObject);
                 }
             }
         } catch (IntrospectionException | IllegalAccessException | InvocationTargetException e) {
-            log.error("Resolving '{}' property for '{}' document has failed for following reason: {}", propertyName, document, e.getMessage());
+            log.error("Resolving '{}' property for '{}' document has failed for following reason: {}", propertyName, ecmObject, e.getMessage());
         }
 
         return null;
     }
 
-    public static void setPropertyValue(Document document, String propertyName, Object propertyValue) {
+    public static void setPropertyValue(EcmObject ecmObject, String propertyName, Object propertyValue) {
+        // TODO: move to ReflectUtils
         try {
-            for (PropertyDescriptor pd : Introspector.getBeanInfo(document.getClass()).getPropertyDescriptors()) {
+            for (PropertyDescriptor pd : Introspector.getBeanInfo(ecmObject.getClass()).getPropertyDescriptors()) {
                 if (pd.getWriteMethod() != null && !"class".equals(pd.getName()) &&
                         pd.getName().equals(propertyName)) {
-                    pd.getWriteMethod().invoke(document, propertyValue);
+                    pd.getWriteMethod().invoke(ecmObject, propertyValue);
                 }
             }
         } catch (IntrospectionException | IllegalAccessException | InvocationTargetException e) {
-            log.error("Setting '{}' property for '{}' document has failed for following reason: {}", propertyName, document, e.getMessage());
+            log.error("Setting '{}' property for '{}' document has failed for following reason: {}", propertyName, ecmObject, e.getMessage());
         }
     }
 }
