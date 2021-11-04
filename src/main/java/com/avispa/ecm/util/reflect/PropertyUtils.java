2304dcc814707d55fed515bdaf6cdb2b6f8163aa
Rafał Hiszpański <rharryh@gmail.com>
2021-11-04 17:06:24 +0100

    Updated PropertyUtils

--- src/main/java/com/avispa/ecm/util/reflect/PropertyUtils.java
+++ src/main/java/com/avispa/ecm/util/reflect/PropertyUtils.java
@@ -1,12 +1,17 @@
 package com.avispa.ecm.util.reflect;
 
-import com.avispa.ecm.model.EcmObject;
+import com.avispa.ecm.model.EcmEntity;
 import lombok.extern.slf4j.Slf4j;
 
+import java.beans.BeanInfo;
 import java.beans.IntrospectionException;
 import java.beans.Introspector;
 import java.beans.PropertyDescriptor;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author Rafał Hiszpański
@@ -18,33 +23,101 @@ public class PropertyUtils {
 
     }
 
-    public static Object getPropertyValue(EcmObject ecmObject, String propertyName) {
-        // TODO: move to ReflectUtils
+    /**
+     * Converts object to map where the key is a property name and value is the field value.
+     *
+     * It returns only fields, which have getter matching bean-standard.
+     * @param object
+     * @return
+     * @throws Exception
+     */
+    public static Map<String, Object> introspect(Object object) {
         try {
-            for (PropertyDescriptor pd : Introspector.getBeanInfo(ecmObject.getClass()).getPropertyDescriptors()) {
-                if (pd.getReadMethod() != null && !"class".equals(pd.getName()) &&
-                        pd.getName().equals(propertyName)) {
-                    return pd.getReadMethod().invoke(ecmObject);
+            Map<String, Object> result = new HashMap<>();
+            BeanInfo info = getBeanInfo(object);
+            for (PropertyDescriptor pd : info.getPropertyDescriptors()) {
+                Method reader = pd.getReadMethod();
+                if (reader != null) {
+                    Object value = getValue(object, reader);
+                    result.put(pd.getName(), value);
                 }
             }
-        } catch (IntrospectionException | IllegalAccessException | InvocationTargetException e) {
-            log.error("Resolving '{}' property for '{}' document has failed for following reason: {}", propertyName, ecmObject, e.getMessage());
+
+            return result;
+        } catch(IntrospectionException e) {
+            log.error("Resolving properties has failed", e);
+        }
+
+        return Collections.emptyMap();
+    }
+
+    /**
+     * Retrieves value of property in provided object.
+     *
+     * It returns only properties, which have getter matching bean-standard.
+     * @param ecmEntity object from which we want to retrieve the value
+     * @param propertyName name of the property for which we want a value
+     * @return value of the property
+     * @throws Exception
+     */
+    public static Object getPropertyValue(EcmEntity ecmEntity, String propertyName) {
+        try {
+            BeanInfo info = getBeanInfo(ecmEntity);
+            for (PropertyDescriptor pd : info.getPropertyDescriptors()) {
+                Method reader = pd.getReadMethod();
+                if (reader != null && !"class".equals(pd.getName()) && pd.getName().equals(propertyName)) {
+                    return getValue(ecmEntity, reader);
+                }
+            }
+        } catch(IntrospectionException e) {
+            log.error("Resolving '{}' property for '{}' entity has failed", propertyName, ecmEntity.getId(), e);
         }
 
         return null;
     }
 
-    public static void setPropertyValue(EcmObject ecmObject, String propertyName, Object propertyValue) {
-        // TODO: move to ReflectUtils
+    private static Object getValue(Object object, Method reader) {
         try {
-            for (PropertyDescriptor pd : Introspector.getBeanInfo(ecmObject.getClass()).getPropertyDescriptors()) {
-                if (pd.getWriteMethod() != null && !"class".equals(pd.getName()) &&
-                        pd.getName().equals(propertyName)) {
-                    pd.getWriteMethod().invoke(ecmObject, propertyValue);
+            return reader.invoke(object);
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            log.error("Error when invoking method {}", reader.getName(), e);
+        }
+        return null;
+    }
+
+    /**
+     * Sets value of property in provided object.
+     *
+     * It works only with classes, which have setter matching bean-standard.
+     * @param ecmEntity object to which we want to set the value
+     * @param propertyName name of the property for which we want to set a value
+     * @param propertyValue value to set
+     * @throws Exception
+     */
+    public static void setPropertyValue(EcmEntity ecmEntity, String propertyName, Object propertyValue) {
+        try {
+            BeanInfo info = getBeanInfo(ecmEntity);
+            for (PropertyDescriptor pd : info.getPropertyDescriptors()) {
+                Method writer = pd.getWriteMethod();
+                if (writer != null && !"class".equals(pd.getName()) && pd.getName().equals(propertyName)) {
+                    setValue(ecmEntity, propertyValue, writer);
                 }
             }
-        } catch (IntrospectionException | IllegalAccessException | InvocationTargetException e) {
-            log.error("Setting '{}' property for '{}' document has failed for following reason: {}", propertyName, ecmObject, e.getMessage());
+        } catch(IntrospectionException e) {
+            log.error("Resolving '{}' property for '{}' entity has failed", propertyName, ecmEntity.getId(), e);
         }
+    }
+
+    private static Object setValue(Object object, Object value, Method writer) {
+        try {
+            return writer.invoke(object, value);
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            log.error("Error when invoking method {}", writer.getName(), e);
+        }
+        return null;
+    }
+
+    private static BeanInfo getBeanInfo(Object obj) throws IntrospectionException {
+        return Introspector.getBeanInfo(obj.getClass());
     }
 }
