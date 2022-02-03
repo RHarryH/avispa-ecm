405a964e466edca25c45f339f8c01885d189faea
Rafał Hiszpański <rharryh@gmail.com>
2022-02-03 09:59:32 +0100

    Combo and radio values can be sorted by label (if dictionary used), by default they are sorted by object name

--- src/main/java/com/avispa/ecm/model/configuration/propertypage/content/control/ComboRadio.java
+++ src/main/java/com/avispa/ecm/model/configuration/propertypage/content/control/ComboRadio.java
@@ -16,6 +16,7 @@ import java.util.UUID;
 public class ComboRadio extends PropertyControl {
     private String objectType;
     private String dictionary;
+    private boolean sortByLabel;
 
     @JsonIgnore
     private List<Map.Entry<UUID, String>> values;
