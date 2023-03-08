168b80e588746607668a45281416c232a928cdc6
Rafał Hiszpański <rharryh@gmail.com>
2023-03-08 15:05:51 +0100

    Configuration is loaded only once when application starts not after each context refresh

--- src/main/java/com/avispa/ecm/model/configuration/load/EcmConfigurationAutoLoader.java
+++ src/main/java/com/avispa/ecm/model/configuration/load/EcmConfigurationAutoLoader.java
@@ -2,8 +2,8 @@ package com.avispa.ecm.model.configuration.load;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Profile;
-import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
@@ -27,7 +27,7 @@ public class EcmConfigurationAutoLoader {
     @Value("${avispa.ecm.configuration.overwrite:false}")
     private boolean overwrite;
 
-    @EventListener(ContextRefreshedEvent.class) // after bean creation but before the server starts
+    @EventListener(ApplicationReadyEvent.class) // after bean creation but before the server starts
     public void loadConfigurations() {
         for(String configurationPath : configurationPaths) {
             configurationLoadService.load(Path.of(configurationPath), overwrite);
