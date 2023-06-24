bb024ad793f6bfc6f21fff5a9f15a363ae8ce5d3
Rafał Hiszpański <rharryh@gmail.com>
2023-06-24 10:38:15 +0200

    Added license text Added more description about the project Added badges at the top of readme file Added basic github workflow

--- src/main/java/com/avispa/ecm/model/configuration/load/EcmConfigurationAutoLoader.java
+++ src/main/java/com/avispa/ecm/model/configuration/load/EcmConfigurationAutoLoader.java
@@ -1,9 +1,26 @@
+/*
+ * Avispa ECM - a small framework for implementing basic ECM solution
+ * Copyright (C) 2023 Rafał Hiszpański
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package com.avispa.ecm.model.configuration.load;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
@@ -16,7 +33,7 @@ import java.nio.file.Path;
  */
 @Component
 @RequiredArgsConstructor
-@Profile({"dev", "test"})
+//@Profile({"dev", "test"})
 public class EcmConfigurationAutoLoader {
 
     private final ConfigurationLoadService configurationLoadService;
