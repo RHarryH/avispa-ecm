c6b58fd848746b0b0ee77cac3dde8415743e7811
Rafał Hiszpański <rharryh@gmail.com>
2023-10-04 13:19:01 +0200

    Each property control has now its own dedicated class so it can be correctly mapped to json Reworked hidden property If value is not found for property, empty string is inserted

--- src/main/java/com/avispa/ecm/model/configuration/propertypage/content/PropertyPageContent.java
+++ src/main/java/com/avispa/ecm/model/configuration/propertypage/content/PropertyPageContent.java
@@ -19,7 +19,6 @@
 package com.avispa.ecm.model.configuration.propertypage.content;
 
 import com.avispa.ecm.model.configuration.propertypage.content.control.Control;
-import com.avispa.ecm.model.configuration.propertypage.content.control.Hidden;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -34,12 +33,4 @@ public class PropertyPageContent {
     private boolean readonly;
     private String size = "normal";
     private List<Control> controls;
-
-    /**
-     * Adds hidden control on the root level of control hierarchy (can't be nested in lists, tables or even columns)
-     * @param property
-     */
-    public void addHiddenControl(String property) {
-        controls.add(new Hidden(property));
-    }
 }
